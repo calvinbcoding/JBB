501cb2376bd08c3a1092ffef9d196fe3654ca471
Jim Haff <Jimmyhaff3@gmail.com>
2019-04-18 12:41:12 -0500

    Added index

--- controllers/authors.js
+++ controllers/authors.js
@@ -7,13 +7,27 @@ router.get('/new', (req, res) => {
   res.render('authors/new.ejs');
 });
 
+// index route
+router.get('/', (req, res) => {
+  // show all of the resource
+  Author.find({}, (err, foundAuthors) => {
+    if(err){
+      res.send(err)
+    } else {
+      res.render('authors/index.ejs', {
+        authors: foundAuthors
+      })
+    }
+  });
+});
+
 router.post('/', (req, res) => {
   console.log(req.body);
   Author.create(req.body, (err, createdAuthor) => {
     if(err){
       res.send(err);
     } else {
-      res.send(createdAuthor)
+      res.redirect('/authors');
     }
   })
 
