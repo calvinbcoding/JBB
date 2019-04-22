c03bf64afeece6a5fe2163602abab61fed040bfc
Jim Haff <Jimmyhaff3@gmail.com>
2019-04-22 15:36:06 -0500

    code pushed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express        = require('express');
 const app            = express();
 const bodyParser     = require('body-parser');
 const methodOverride = require('method-override');
+const session        = require('express-session');
 require('./db/db')
 
 const authorsController  = require('./controllers/authors');
@@ -11,6 +12,17 @@ const authController     = require('./controllers/authController');
 // of our requests readable then send it to the next function
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(methodOverride('_method'));
+
+// before our controllers
+app.use(session({
+  secret: 'This is a random secret string that you would make up to protect your session',
+  resave: false, // says only save the cookie if there has been a change to one of properties
+  saveUninitialized: false // only save when we have mutated the session,
+  //this is what should be done for logins, many laws make you do this as well
+}))
+
+
+
 
 
 app.use('/authors', authorsController);
