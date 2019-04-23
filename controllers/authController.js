d3d3b131e85b60ec361b797e11721f9011ad8e5a
Jim Haff <Jimmyhaff3@gmail.com>
2019-04-23 10:59:03 -0500

    added user model"

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,20 +1,6 @@
 const express = require('express');
 const router = express.Router();
-
-// this is what the session looks like on the client
-// 's%3ABwt2E_dsM-nCo-c4S4JN4b0RLKRT5msC.s8%2BbdNFBLnpBKyC6DMuIRgfanpd%2BFvYtH8RZQedndPY'
-
-router.get('/test', (req, res) => {
-  console.log("=========================")
-  console.log(req.session);
-  console.log("=========================")
-  req.session.myOwnPropertyIMadeUp = 'Cheese';
-  console.log("=========================")
-  console.log(req.session);
-  console.log("=========================")
-  res.send('hi test')
-});
-
+const User   = require('../models/users');
 
 router.get('/login', (req, res) => {
   res.render('login.ejs')
