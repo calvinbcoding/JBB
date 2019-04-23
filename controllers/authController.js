c2dd28e6ab91646cc6cf15b94117dfdbe729a041
Jim Haff <Jimmyhaff3@gmail.com>
2019-04-23 11:33:24 -0500

    added register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,56 @@
 const express = require('express');
 const router = express.Router();
 const User   = require('../models/users');
+const bcrypt = require('bcryptjs');
+
 
 router.get('/login', (req, res) => {
   res.render('login.ejs')
 });
+
+
+// Creating what is called a hash - which is an encrypted string, based on a really
+// complex mathmatical formula
+
+
+// hash string
+
+// Salt is like a key to your hash
+
+// hash and salt get combined
+
+// const hashedString = bcyrpt.hashSync('Your Password here', bcrypt.genSaltSync(10));
+router.post('/register', async (req, res) => {
+
+  // First we must hash the password
+  const password = req.body.password;
+  // The password hash is what we want to put in the Database
+  const passwordHash = bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+
+  // create an object for the db entry, the properties of this object
+  // will match up with our model
+  const userDbEntry = {};
+  userDbEntry.username = req.body.username;
+  userDbEntry.password = passwordHash;
+
+  try {
+    const createdUser = await User.create(userDbEntry);
+
+    console.log(createdUser);
+
+    res.send('you registered');
+
+  } catch(err){
+    res.send(err)
+  }
+
+
+
+});
+
+
+
 
 // make the form in login.ejs make a request to this
 router.post('/login', (req, res) => {
